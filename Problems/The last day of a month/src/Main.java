828733022e703acb60c90e7a65e40dc3dab86751
vidyut07 <vidyut07@gmail.com>
2020-06-19 01:33:27 -0400

    LocalDate

--- Problems/The last day of a month/src/Main.java
+++ Problems/The last day of a month/src/Main.java
@@ -1,5 +1,26 @@
-gitclass Main {
+import java.time.LocalDate;
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
         // put your code here
-    }
+
+        // put your code here
+        Scanner in = new Scanner(System.in);
+        int year = in.nextInt();
+        int dayOfYear = in.nextInt();
+
+        LocalDate date1 = LocalDate.of(year,1,1);
+        LocalDate date2=date1.plusDays(dayOfYear-1);
+        // System.out.println(date1);
+        //  System.out.println(date2);
+        if(date2.getDayOfMonth()==date2.lengthOfMonth()) {
+        System.out.println("true");
+        }
+        else {
+        System.out.println("false");
+        }
+
+        }
+
 }

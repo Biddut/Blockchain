2adb09ec9850b7212f4cd5360f76a71233654845
vidyut07 <vidyut07@gmail.com>
2020-06-19 03:00:07 -0400

    avoid NPE

--- Problems/Avoid NPE/src/Main.java
+++ Problems/Avoid NPE/src/Main.java
@@ -8,6 +8,6 @@ class FixingNullPointerException {
         string = "null".equals(string) ? null : string;
         /* Do not change code above */
 
-        System.out.println(string.toLowerCase());
+        System.out.println(string!=null?string.toLowerCase():"NPE!");
     }
 }

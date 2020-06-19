c5c43a5561884ae7daedf149894ed196344980e2
vidyut07 <vidyut07@gmail.com>
2020-06-19 02:55:39 -0400

    NPE

---
+++ Problems/Some hours and minutes ago/src/Main.java
@@ -0,0 +1,15 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        Scanner in = new Scanner(System.in);
+        int seconds = in.nextInt();
+
+        LocalTime localtime = LocalTime.ofSecondOfDay(seconds);
+        System.out.println(localtime);
+
+    }
+}

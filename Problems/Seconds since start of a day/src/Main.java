e98fe479405bde0ec945a058ea838deb171f0e0f
vidyut07 <vidyut07@gmail.com>
2020-06-19 02:02:50 -0400

    LocaTime

---
+++ Problems/Seconds since start of a day/src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        Scanner in = new Scanner(System.in);
+        int seconds = in.nextInt();
+
+        LocalTime localtime = LocalTime.ofSecondOfDay(seconds);
+        System.out.println(localtime);
+
+    }
+}

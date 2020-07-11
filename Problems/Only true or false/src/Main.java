1285b6e1e0c2bf7b3860c64ce8e9e89638bed7fd
vidyut07 <vidyut07@gmail.com>
2020-07-11 00:44:27 -0400

    boxing unboxing

---
+++ Problems/Only true or false/src/Main.java
@@ -0,0 +1,9 @@
+class Primitive {
+    public static boolean toPrimitive(Boolean b) {
+
+        boolean bool = b!=null?b:false;
+
+
+        return bool;
+    }
+}
--- Problems/Some hours and minutes ago/src/Main.java
+++ Problems/Some hours and minutes ago/src/Main.java
@@ -1,15 +1,5 @@
-import java.time.LocalTime;
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         // put your code here
-
-        Scanner in = new Scanner(System.in);
-        int seconds = in.nextInt();
-
-        LocalTime localtime = LocalTime.ofSecondOfDay(seconds);
-        System.out.println(localtime);
-
     }
 }

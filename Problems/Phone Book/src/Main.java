9fe1076146ba549820620c9bc79b1ea06229306c
vidyut07 <vidyut07@gmail.com>
2020-07-30 02:50:09 -0400

    phonebook hastable

--- Problems/Phone Book/src/Main.java
+++ Problems/Phone Book/src/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+
     private static class TableEntry<T> {
         private final int key;
-        private final T value;
+        private  T value;
         private boolean removed;
 
         public TableEntry(int key, T value) {
             this.key = key;
+            this.value = value;
+        }
+
+        public void setValue(T value) {
             this.value = value;
         }
 
@@ -35,24 +42,110 @@ public class Main {
             table = new TableEntry[size];
         }
 
-        public boolean put(int key, T value) {
-            // put your code here
+        private int findKey(int key) {
+            int hash = key % size;
+
+            while (!(table[hash] == null || table[hash].getKey() == key)) {
+                hash = (hash + 1) % size;
+
+                if (hash == key % size) {
+                    return -1;
+                }
+            }
+
+            return hash;
         }
 
+        public boolean put(int key, T value) {
+            TableEntry entry = getEntry(key);
+            if (entry != null) {
+                entry.setValue(entry.getValue() + " " + value);
+                return true;
+            }
+            int idx = findKey(key);
+
+            if (idx == -1) {
+                return false;
+            }
+
+            table[idx] = new TableEntry(key, value);
+            return true;
+        }
+
+        public TableEntry getEntry(int key) {
+            if (table.length > 0) {
+                for (TableEntry e : table
+                ) {
+                    if (e != null && e.getKey() == key) {
+                        return e;
+                    }
+                }
+            }
+            return null;
+        }
+
+
+
         public T get(int key) {
-            // put your code here
+            int idx = findKey(key);
+
+            if (idx == -1 || table[idx] == null) {
+                return null;
+            }
+
+            return (T) table[idx].getValue();
         }
 
         public void remove(int key) {
-            // put your code here
+            int idx = findKey(key);
+            table[idx] = null;
+
+            if (idx == -1 || table[idx] == null) {
+                return;
+            }
         }
 
-        private int findKey(int key) {
-            // put your code here
+        @Override
+        public String toString() {
+            StringBuilder tableStringBuilder = new StringBuilder();
+
+            for (int i = 0; i < table.length; i++) {
+                if (table[i] != null) {
+                    tableStringBuilder.append(table[i].getValue());
+                    if (i < table.length - 1) {
+                        tableStringBuilder.append("\n");
+                    }
+                }
+
+            }
+
+            return tableStringBuilder.toString();
         }
+
+
     }
 
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        HashTable<String> table = new HashTable(num);
+        for (int i = 0; i < num; i++) {
+            String[] command = scanner.nextLine().split(" ");
+          //  table.put(Integer.parseInt(command[0]), command[1]);
+            if(command[0].equals("put")){
+                table.put(Integer.parseInt(command[1]), command[2]);
+            }
+            else if(command[0].equals("get")){
+               //String value= table.get(Integer.parseInt(command[1]));
+               System.out.println(table.get(Integer.parseInt(command[1]))==null?-1:table.get(Integer.parseInt(command[1])));
+            }
+            else if(command[0].equals("remove")){
+                table.remove(Integer.parseInt(command[1]));
+            }
+
+        }
+
+     //   System.out.println(table);
     }
+
 }
--- Problems/Removing/src/Main.java
+++ Problems/Removing/src/Main.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     private static class TableEntry<T> {
         private final int key;
-        private final T value;
+        private  T value;
         private boolean removed;
 
         public TableEntry(int key, T value) {
             this.key = key;
             this.value = value;
         }
-
+        public void setValue(T value) {
+            this.value = value;
+        }
         public int getKey() {
             return key;
         }
@@ -18,11 +22,11 @@ public class Main {
         }
 
         public void remove() {
-             removed = true;   
+            removed = true;
         }
 
         public boolean isRemoved() {
-             return removed;   
+            return removed;
         }
     }
 
@@ -34,22 +38,69 @@ public class Main {
             this.size = size;
             table = new TableEntry[size];
         }
+        private int findEntryIndex(int key) {
+            int hash = key % size;
+
+            while (!(table[hash] == null || table[hash].getKey() == key)) {
+                hash = (hash + 1) % size;
+
+                if (hash == key % size) {
+                    return -1;
+                }
+            }
+
+            return hash;
+        }
 
         public boolean put(int key, T value) {
-            // put your code here
+            TableEntry entry = getEntry(key);
+            if (entry != null) {
+                entry.setValue(entry.getValue() + " " + value);
+                return true;
+            }
+            int idx = findEntryIndex(key);
+
+            if (idx == -1) {
+                return false;
+            }
+
+            table[idx] = new TableEntry(key, value);
+            return true;
+        }
+
+        public TableEntry getEntry(int key) {
+            if (table.length > 0) {
+                for (TableEntry e : table
+                ) {
+                    if (e != null && e.getKey() == key) {
+                        return e;
+                    }
+                }
+            }
+            return null;
         }
 
         public T get(int key) {
-            // put your code here
+            int idx = findEntryIndex(key);
+
+            if (idx == -1 || table[idx] == null) {
+                return null;
+            }
+
+            return (T) table[idx].getValue();
         }
 
         public void remove(int key) {
-            // put your code here
+            int idx = findEntryIndex(key);
+
+
+            if (idx == -1 ) {
+                return;
+            }
+            else table[idx].remove();
+            // return;
         }
 
-        private int findKey(int key) {
-            // put your code here
-        }
 
         private void rehash() {
             // put your code here
@@ -64,8 +115,8 @@ public class Main {
                     tableStringBuilder.append(i + ": null");
                 } else {
                     tableStringBuilder.append(i + ": key=" + table[i].getKey()
-                                                + ", value=" + table[i].getValue()
-                                                + ", removed=" + table[i].isRemoved());
+                            + ", value=" + table[i].getValue()
+                            + ", removed=" + table[i].isRemoved());
                 }
 
                 if (i < table.length - 1) {
@@ -78,6 +129,24 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] nm = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        HashTable<String> table = new HashTable(n);
+        //  System.out.println(n+" "+m);
+
+        for (int i = 0; i < n; i++) {
+            String[] command = scanner.nextLine().split(" ");
+            // System.out.println(command[0]+" "+command[1]);
+            table.put(Integer.parseInt(command[0]), command[1]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int rKey = Integer.parseInt(scanner.nextLine());
+            table.remove(rKey);
+
+        }
+        System.out.println(table);
     }
 }
